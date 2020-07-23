f2ab1b2029b4b297bfe7172ff8ecfb84aa686901
Thanh Huynh <46855681+thuynh323@users.noreply.github.com>
2020-07-22 20:15:48 -0400

    Add a function for convenience

---
+++ FuzzyWuzzy - Ramen Rater List/fuzz_match_function.py
@@ -0,0 +1,15 @@
+def fuzz_match(unique_list, scorer, threshold):
+    score = [(x,) + i
+             for x in unique_list
+             for i in process.extract(x, unique_list, scorer=scorer)]
+    similarity = pd.DataFrame(score, columns=['brand','match','score'])
+    similarity['sorted_brand'] = np.minimum(similarity['brand'], similarity['match'])
+    
+    high_score = similarity[(similarity['score'] >= threshold) &
+                            (similarity['brand'] != similarity['match']) &
+                            (similarity['sorted_brand'] != similarity['match'])]
+    high_score = high_score.drop('sorted_brand', axis=1)
+    result = high_score.groupby(['brand','score']).agg(
+                                {'match': ', '.join}).sort_values(
+                                ['score'], ascending=False)
+    return result
