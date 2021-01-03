e38a8ca748b0c769236085970e4a4d9517898ac9
Thanh Huynh <46855681+thuynh323@users.noreply.github.com>
2021-01-03 12:48:49 -0500

    Update fuzz_match_function.py

--- FuzzyWuzzy - Ramen Rater List/fuzz_match_function.py
+++ FuzzyWuzzy - Ramen Rater List/fuzz_match_function.py
@@ -2,35 +2,35 @@ import pandas as pd
 import numpy as np
 from fuzzywuzzy import process, fuzz
 
-ramen = pd.read_excel('The-Ramen-Rater-The-Big-List-1-3400-Current-As-Of-Jan-25-2020.xlsx')
-
-# Get a list of unique values
-unique_brand = ramen['Brand'].unique().tolist()
-
-def get_score(scorer, threshold):
-	"""
-	Return a table of each brand name, its corresponding
-	similar names found in the list, and their score
-
-	Parameters:
-	scorer - the selected scorer (fuzz.token_sort_ratio, fuzz.token_set_ratio)
-	threshold - the cut-off score
-	"""
-
+def get_score(unique_values, scorer, threshold):
+    """
+    Return a table of each brand name, its corresponding
+    similar names found in the list, and their score
+    Parameters:
+    unique_value - the list of unique values
+    scorer - the selected scorer (fuzz.token_sort_ratio, fuzz.token_set_ratio)
+    threshold - the cut-off score
+    """
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_colwidth', None)
+    
     # Create tuples of brand names, matched brand names, and the score
     score = [(x,) + i
-             for x in unique_brand
-             for i in process.extract(x, unique_brand, scorer=scorer)]
+             for x in unique_values
+             for i in process.extract(x, unique_values, scorer=scorer)]
     
     # Create dataframe from the tuples
-    similarity = pd.DataFrame(score, columns=['brand','match','score'])
+    similarity = pd.DataFrame(score, columns=['value','match','score'])
     
     # Derive representative values
-    similarity['sorted_brand'] = np.minimum(similarity['brand'], similarity['match'])
+    similarity['sorted_value'] = np.minimum(similarity['value'], similarity['match'])
     high_score = similarity[(similarity['score'] >= threshold) &
-                            (similarity['brand'] != similarity['match']) &
-                            (similarity['sorted_brand'] != similarity['match'])]
+                            (similarity['value'] != similarity['match']) &
+                            (similarity['sorted_value'] != similarity['match'])]
 
     # Group matches by brand names and scores
-    result = high_score.groupby(['brand','score']).agg({'match': ', '.join}).sort_values(['score'], ascending=False)
+    if scorer == fuzz.token_sort_ratio:
+        result = high_score.groupby(['value','score']).agg({'match': ', '.join}).sort_values(['score'], ascending=False)
+    if scorer == fuzz.token_set_ratio:
+        result = high_score.groupby(['match','score']).agg({'value': ', '.join}).sort_values(['score'], ascending=False)
     return result
